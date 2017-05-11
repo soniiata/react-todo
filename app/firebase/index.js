a87dc54b7df668bf921d0efd4c4bf052ff74e364
Soni Tri Gustiono <sonitrigustiono92@gmail.com>
2017-05-11 23:16:22 +0700

    Add node-env

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,10 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyAl45khSi3uM7e_xQRocyQkwhkPKc6RZlg",
-    authDomain: "mead-todo-app-f3b67.firebaseapp.com",
-    databaseURL: "https://mead-todo-app-f3b67.firebaseio.com",
-    projectId: "mead-todo-app-f3b67",
-    storageBucket: "mead-todo-app-f3b67.appspot.com",
-    messagingSenderId: "173708255170"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
 
   firebase.initializeApp(config);
